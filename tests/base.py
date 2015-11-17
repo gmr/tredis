3767dc4fc9b13521548c3f30a8e2a49e050c56d8
Gavin M. Roy <gavinr@aweber.com>
2015-11-17 15:52:31 -0500

    Add some convenience functions

--- tests/base.py
+++ tests/base.py
@@ -2,6 +2,7 @@ import os
 import uuid
 
 from tornado import concurrent
+from tornado import gen
 from tornado import testing
 
 import tredis
@@ -11,12 +12,20 @@ import tredis
 
 class AsyncTestCase(testing.AsyncTestCase):
 
+    DEFAULT_EXPIRATION = 5
+
     def setUp(self):
         super(AsyncTestCase, self).setUp()
         self.client = tredis.RedisClient(os.getenv('REDIS_HOST', 'localhost'),
                                          int(os.getenv('REDIS_PORT', '6379')),
                                          int(os.getenv('REDIS_DB', '0')))
         self._execute_result = None
+
+    @gen.coroutine
+    def expiring_set(self, key, value, expiration=None):
+        result = yield self.client.set(key, value,
+                                       expiration or self.DEFAULT_EXPIRATION)
+        raise gen.Return(result)
 
     def _execute(self, parts, callback):
         future = concurrent.Future()
@@ -26,5 +35,9 @@ class AsyncTestCase(testing.AsyncTestCase):
         else:
             future.set_result(self._execute_result)
 
-    def uuid4(self):
-        return str(uuid.uuid4()).encode('ascii')
+    def uuid4(self, qty=1):
+        if qty == 1:
+            return str(uuid.uuid4()).encode('ascii')
+        else:
+            return tuple([str(uuid.uuid4()).encode('ascii')
+                          for i in range(0, qty)])
