92b7fc6add734ff39ffbae78dca8fdc16a02b4a9
Gavin M. Roy <gavinr@aweber.com>
2015-11-18 20:18:55 -0500

    Add core functionality to enable pipelining

--- tredis/__init__.py
+++ tredis/__init__.py
@@ -45,6 +45,29 @@ class _RESPArrayNamespace(object):
         self.values = []
 
 
+class _PipelineResponses(object):
+    """Class for returning pipeline responses"""
+
+    def __init__(self):
+        self.values = []
+
+    def __len__(self):
+        """Return the length of the pipeline responses
+
+        :rtype: int
+
+        """
+        return len(self.values)
+
+    def append(self, value):
+        """Add a value to the pipeline response list
+
+        :param mixed value: The value to append
+
+        """
+        self.values.append(value)
+
+
 class RedisClient(
         server.ServerMixin, keys.KeysMixin, strings.StringsMixin, geo.GeoMixin,
         hashes.HashesMixin, hyperloglog.HyperLogLogMixin, lists.ListsMixin,
@@ -88,6 +111,8 @@ class RedisClient(
         self._ioloop = ioloop.IOLoop.current()
         self._on_close_callback = on_close_callback
         self._stream = None
+        self._pipeline = False
+        self._pipeline_commands = []
         super(RedisClient, self).__init__()
 
     def connect(self):
@@ -129,6 +154,82 @@ class RedisClient(
         """Close the connection to the Redis Server"""
         self._stream.close()
 
+    def pipeline_start(self):
+        """Start a command pipeline. The pipeline will only run when you invoke
+        :py:meth:`pipeline_execute <tredis.RedisClient.pipeline_execute`.
+
+        **Example:**
+
+        .. code::
+
+            # Start the pipeline
+            client.pipeline_start()
+
+            client.set('foo1', 'bar1')
+            client.set('foo2', 'bar2')
+            client.set('foo3', 'bar3')
+
+            # Execute the pipeline
+            responses = yield client.pipeline_execute()
+
+        .. note:: While the client sends commands using pipelining, the server
+           will be forced to queue the replies, using memory. So if you need
+           to send a lot of commands with pipelining, it is better to send
+           them as batches having a reasonable number, for instance 10k
+           commands, read the replies, and then send another 10k commands
+           again, and so forth. The speed will be nearly the same, but the
+           additional memory used will be at max the amount needed to queue the
+           replies for this 10k commands.
+
+        """
+        self._pipeline = True
+        self._pipeline_commands = []
+
+    def pipeline_execute(self):
+        """Execute the pipeline created by issuing commands after invoking
+        :py:meth:`pipeline_start <tredis.RedisClient.pipeline_start`.
+
+        :raises: ValueError
+
+        """
+        commands = len(self._pipeline_commands)
+        if not commands:
+            raise ValueError('Empty pipeline')
+        pipeline_responses = _PipelineResponses()
+
+        future = concurrent.TracebackFuture()
+
+        def on_response(response):
+            """Process the response future
+
+            :param response: The response future
+            :type response: :py:class:`tornado.concurrent.Future`
+
+            """
+            exc = response.exception()
+            if exc:
+                pipeline_responses.append(exc)
+            else:
+                index = len(pipeline_responses.values)
+                eval_method = self._pipeline_commands[index][1]
+                if eval_method:
+                    pipeline_responses.append(eval_method(response.result()))
+                else:
+                    pipeline_responses.append(response.result())
+
+            if len(pipeline_responses) == commands:
+                future.set_result(pipeline_responses.values)
+            else:
+                self._get_response(on_response)
+
+        def on_written():
+            """Invoked when the command has been written to the socket"""
+            self._get_response(on_response)
+
+        pipeline = b''.join([cmd[0] for cmd in self._pipeline_commands])
+        self._stream.write(pipeline, callback=on_written)
+        return future
+
     def _build_command(self, parts):
         """Build the command that will be written to Redis via the socket
 
@@ -168,6 +269,9 @@ class RedisClient(
         :rtype: :py:class:`tornado.concurrent.Future`
 
         """
+        if self._pipeline:
+            raise exceptions.PipelineError()
+
         future = concurrent.TracebackFuture()
         self._ioloop.add_future(future, callback)
 
@@ -216,24 +320,6 @@ class RedisClient(
 
         self._execute(parts, on_response)
         return future
-
-    @staticmethod
-    def _is_ok(response, future):
-        """Method invoked in a lambda to abbreviate the amount of code in
-        each method when checking for an ``OK`` response.
-
-        :param response: The RedisClient._execute future
-        :type response: :py:class:`tornado.concurrent.Future`
-        :param concurrent.Future future: The current method's future
-        :type future: :py:class:`tornado.concurrent.Future`
-
-        """
-        exc = response.exception()
-        if exc:
-            future.set_exception(exc)
-        else:
-            result = response.result()
-            future.set_result(result == b'OK')
 
     def _get_response(self, callback):
         """Read and parse command execution responses from Redis
@@ -331,10 +417,10 @@ class RedisClient(
                     """Process the response segment data
 
                     :param response: The response array segment data
-                    :type response: bytes
+                    :type response: int
 
                     """
-                    self._read_array(response, on_complete)
+                    self._get_array(response, on_complete)
 
                 self._stream.read_until(CRLF, callback=on_segments)
 
@@ -344,7 +430,7 @@ class RedisClient(
 
         self._stream.read_bytes(1, callback=on_first_byte)
 
-    def _read_array(self, segments, callback):
+    def _get_array(self, segments, callback):
         """Read in an array by calling :py:meth:`RedisClient._get_response`
         for each element in the array.
 
@@ -373,8 +459,57 @@ class RedisClient(
 
         self._get_response(on_response)
 
+    @staticmethod
+    def _is_ok(response, future):
+        """Method invoked in a lambda to abbreviate the amount of code in
+        each method when checking for an ``OK`` response.
+
+        :param response: The RedisClient._execute future
+        :type response: :py:class:`tornado.concurrent.Future`
+        :param concurrent.Future future: The current method's future
+        :type future: :py:class:`tornado.concurrent.Future`
+
+        """
+        exc = response.exception()
+        if exc:
+            future.set_exception(exc)
+        else:
+            result = response.result()
+            future.set_result(result == b'OK')
+
     def _on_closed(self):
         """Invoked when the connection is closed"""
         LOGGER.error('Connection closed')
         if self._on_close_callback:
             self._on_close_callback()
+
+    def _pipeline_add(self, parts, response_processor=None):
+        """Add a command to execute to the pipeline
+
+        :param list parts: The command parts
+        :param method response_processor: A method for command response eval
+
+        """
+        self._pipeline_commands.append((self._build_command(parts),
+                                        response_processor))
+
+    @staticmethod
+    def _pipeline_int_is_1(value):
+        """Method invoked when evaluating a pipeline response looking for
+        the value of 1
+
+        :param int value: The Redis response to evaluate
+        :rtype: bool
+
+        """
+        return value == 1
+
+    @staticmethod
+    def _pipeline_is_ok(value):
+        """Method invoked when evaluating a pipeline response for b'OK'
+
+        :param bytes value: The Redis response to evaluate
+        :rtype: bool
+
+        """
+        return value == b'OK'
