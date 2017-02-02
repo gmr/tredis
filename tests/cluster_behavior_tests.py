546f90093306c517ba801a5ecebc29b947cd53e4
Gavin M. Roy <gavinr@aweber.com>
2017-02-02 18:43:31 -0500

    Get some debug out put for this issue in Travis

--- tests/cluster_behavior_tests.py
+++ tests/cluster_behavior_tests.py
@@ -1,9 +1,10 @@
-import logging
 import os
+import pprint
 import uuid
 
-import mock
-from tornado import gen, testing
+from tornado import testing
+
+import tredis
 
 from . import base
 
@@ -25,8 +26,14 @@ class ClusterBehaviorTests(AsyncTestCase):
     @testing.gen_test
     def test_connection_move(self):
         yield self.client.connect()
-        for offset in range(0, 20):
-            key = str(uuid.uuid4())
-            yield self.client.set(key, b'1', ex=10)
-            value = yield self.client.get(key)
-            self.assertEqual(value, b'1')
+        try:
+            for offset in range(0, 20):
+                key = str(uuid.uuid4())
+                yield self.client.set(key, b'1', ex=10)
+                value = yield self.client.get(key)
+                self.assertEqual(value, b'1')
+        except tredis.RedisError as error:
+            info = yield self.cluster_info()
+            pprint.pprint(info)
+            raise
+
